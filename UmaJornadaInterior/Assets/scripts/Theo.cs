28282c1f13221bcff4b9d041e794a1c5fa9cce7c
GabrielaBeatriz <129885028+GabrielaBeatriz@users.noreply.github.com>
2024-10-09 10:26:41 -0300

    theo finalizado

--- UmaJornadaInterior/Assets/scripts/Theo.cs
+++ UmaJornadaInterior/Assets/scripts/Theo.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class Theo : MonoBehaviour
 {
-    
     public float Speed;
     public float StoopingDistance;
-    private Transform Target; 
-    
+    private Transform Target;
+    private SpriteRenderer spriteRenderer;
+
     // Start is called before the first frame update
     void Start()
     {
         Target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -21,7 +22,18 @@ public class Theo : MonoBehaviour
         if (Vector2.Distance(transform.position, Target.position) > StoopingDistance)
         {
             transform.position = Vector2.MoveTowards(transform.position, Target.position, Speed * Time.deltaTime);
+            
+            // Verifica a direção do jogador em relação ao Theo
+            if (Target.position.x > transform.position.x)
+            {
+                // O jogador está à direita
+                spriteRenderer.flipX = false; // Olhar para a direita
+            }
+            else
+            {
+                // O jogador está à esquerda
+                spriteRenderer.flipX = true; // Olhar para a esquerda
+            }
         }
     }
-
 }
