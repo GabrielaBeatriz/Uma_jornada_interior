acdf90490c6b17c78528bbd20558b1f20df0e992
GabrielaBeatriz <129885028+GabrielaBeatriz@users.noreply.github.com>
2024-10-17 09:39:27 -0300

    espinho

---
+++ UmaJornadaInterior/Assets/scripts/espinho.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class espinho : MonoBehaviour
+{
+    
+    public Vector2 intervalo; 
+    private Animator anim; 
+    
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        anim.enabled = false;
+    }
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(Random.Range(intervalo.x, intervalo.y));
+        anim.enabled = true;
+    }
+
+}
